7928cc8527594a301420d4a8dd53e122906d95be
pg147 <xyzprathamesh147@gmail.com>
2025-01-02 23:33:12 +0530

    onClick handled

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,12 +1,13 @@
 import clsx from "clsx";
 
 interface Props extends React.PropsWithChildren {
+    onClick? : () => void;
     className? : string;
 }
 
-const Cell: React.FC<Props> = ({className, children}) => {
+const Cell: React.FC<Props> = ({className, onClick, children}) => {
     return(
-        <div className={clsx("p-2 rounded-xl cursor-pointer font-medium hover:bg-tile", className)}>{children}</div>
+        <div onClick={onClick} className={clsx("p-2 rounded-xl text-center cursor-pointer font-medium hover:bg-tile", className)}>{children}</div>
     )
 }
 
